b4237f40b64a31bd79745c5cbb84d81c2218106f
Aditya kumar <aditechhelp@gmail.com>
2023-07-13 21:48:13 +0530

    Basic Schema For Portfolio

--- schemas/index.js
+++ schemas/index.js
@@ -1,1 +1,6 @@
-export const schemaTypes = []
+import skills from './skills'
+import abouts from './abouts'
+import contact from './contact'
+import works from './works'
+import testimonials from './testimonials'
+export const schemaTypes = [skills,abouts,contact,works,testimonials]
